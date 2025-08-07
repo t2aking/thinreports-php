1c89766143daee66c06851fbf399f864361bc365
Tatsuaki Morioka <racerx0075440@gmail.com>
2025-08-07 15:26:24 +0900

    Refactor TextFormatter to improve date formatting and add convertStrftimeToDateFormat method

--- src/Thinreports/Item/TextFormatter.php
+++ src/Thinreports/Item/TextFormatter.php
@@ -83,7 +83,7 @@ class TextFormatter
         $datetime = date_create($value);
 
         if ($datetime) {
-            return strftime($datetime_format['format'], date_timestamp_get($datetime));
+            return $datetime->format($this->convertStrftimeToDateFormat($datetime_format['format']));
         }
 
         return $value;
@@ -161,5 +161,73 @@ class TextFormatter
         }
 
         return $string;
+    }
+
+    public function convertStrftimeToDateFormat(string $format): string
+    {
+        $replacements = [
+            // 年
+            '%Y' => 'Y',    // 4桁の年 (例: 2023)
+            '%y' => 'y',    // 2桁の年 (例: 23)
+            '%C' => '',     // 世紀 - DateTime::formatには直接対応なし
+
+            // 月
+            '%B' => 'F',    // 月名フル (例: January)
+            '%b' => 'M',    // 月名省略 (例: Jan)
+            '%h' => 'M',    // 月名省略 (%bと同じ)
+            '%m' => 'm',    // 月 (01-12)
+
+            // 日
+            '%d' => 'd',    // 日 (01-31)
+            '%e' => 'j',    // 日 (1-31) - ゼロパディングなし
+            '%j' => 'z',    // 年間通算日 (001-366) ※注：DateTime::formatでは0-365
+
+            // 週
+            '%A' => 'l',    // 曜日名フル (例: Sunday)
+            '%a' => 'D',    // 曜日名省略 (例: Sun)
+            '%w' => 'w',    // 曜日番号 (0=日曜日)
+            '%u' => 'N',    // 曜日番号 (1=月曜日)
+            '%U' => '',     // 年間通算週 (日曜始まり) - DateTime::formatには直接対応なし
+            '%W' => 'W',    // 年間通算週 (月曜始まり)
+            '%V' => 'W',    // ISO週番号
+
+            // 時間
+            '%H' => 'H',    // 時 (00-23)
+            '%k' => 'G',    // 時 (0-23) - ゼロパディングなし
+            '%I' => 'h',    // 時 (01-12)
+            '%l' => 'g',    // 時 (1-12) - ゼロパディングなし
+            '%M' => 'i',    // 分 (00-59)
+            '%S' => 's',    // 秒 (00-59)
+            '%p' => 'A',    // AM/PM
+            '%P' => 'a',    // am/pm
+            '%r' => 'h:i:s A', // 12時間形式の時刻 (例: 09:05:42 PM)
+            '%R' => 'H:i',  // 24時間形式の時分 (例: 21:05)
+            '%T' => 'H:i:s', // 24時間形式の時刻 (例: 21:05:42)
+            '%X' => 'H:i:s', // 時刻表現 (ロケール依存)
+
+            // 日付
+            '%D' => 'm/d/y', // MM/DD/YY形式
+            '%F' => 'Y-m-d', // YYYY-MM-DD形式
+            '%x' => 'n/j/Y', // 日付表現 (ロケール依存)
+
+            // 日付時刻
+            '%c' => 'D M j H:i:s Y', // 完全な日時表現
+            '%s' => 'U',    // Unixタイムスタンプ
+
+            // タイムゾーン
+            '%z' => 'O',    // タイムゾーンオフセット (+0900)
+            '%Z' => 'T',    // タイムゾーン名 (JST)
+
+            // その他
+            '%%' => '%',    // パーセント文字そのもの
+            '%n' => "\n",   // 改行
+            '%t' => "\t",   // タブ
+
+            // 以下は DateTime::format に直接対応するものがない
+            '%g' => '',     // ISO年の下2桁
+            '%G' => '',     // ISO年
+        ];
+
+        return strtr($format, $replacements);
     }
 }
